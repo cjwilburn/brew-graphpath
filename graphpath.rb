755e11fa8bbe4a3fd8e770722d9d7cdfe908e8f0
Courtney Wilburn <courtney@cjwilburn.com>
2018-05-16 20:52:57 -0400

    getting rid of comments

--- graphpath.rb
+++ graphpath.rb
@@ -1,34 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Graphpath < Formula
   desc "Graphpath generates an ASCII network diagram from the route table of a Unix/Linux"
   homepage "https://bsdrp.net/"
   url "https://github.com/ocochard/graphpath/archive/v1.0.tar.gz"
   sha256 "9e018bbb44f748595d4eb030896ccc195a9a75db773160da480743f31b5b8272"
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
+
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test graphpath`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "script", "-q", "/dev/null", bin/"graphpath"
   end
 end
