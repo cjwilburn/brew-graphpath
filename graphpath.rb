1cd994bb32fd62ec4763012d353ff8767508ce2d
Courtney Wilburn <courtney@cjwilburn.com>
2018-05-16 20:54:34 -0400

    updating description

--- graphpath.rb
+++ graphpath.rb
@@ -1,5 +1,5 @@
 class Graphpath < Formula
-  desc "Graphpath generates an ASCII network diagram from the route table of a Unix/Linux"
+  desc "generates an ASCII network diagram from the route table of a Unix/Linux"
   homepage "https://bsdrp.net/"
   url "https://github.com/ocochard/graphpath/archive/v1.0.tar.gz"
   sha256 "9e018bbb44f748595d4eb030896ccc195a9a75db773160da480743f31b5b8272"
